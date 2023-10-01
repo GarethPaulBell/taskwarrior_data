914e1eaedf675a16acf940272bf9870f72d55914
garethpaulbell <gareth.paul.bell@gmail.com>
2023-10-01 14:00:01 +1300

    Automated backup Sun Oct  1 14:00:01 NZDT 2023

---
+++ sync_taskwarrior.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define the source and destination directories
+SOURCE_DIR=~/.task
+DEST_DIR=~/taskwarrior_data
+
+# Create the destination directory if it doesn't exist
+mkdir -p "$DEST_DIR"
+
+# Copy the Taskwarrior data
+cp -r "$SOURCE_DIR"/* "$DEST_DIR"/
+
+# Optionally, sync the data if you have a remote repository
+git -C "$DEST_DIR" add .
+git -C "$DEST_DIR" commit -m "Automated backup $(date)"
+git -C "$DEST_DIR" push origin main
+
