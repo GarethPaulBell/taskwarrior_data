bbd2979dcc703d158698764c07e962d6eaf31bc6
garethpaulbell <gareth.paul.bell@gmail.com>
2023-10-01 21:53:19 +1300

    aider: Added check to only push repository if changes have been made.
    # Aider chat conversation:
    
    USER: please change so that the repository is pushed only if something has changed
    
    ASSISTANT: To achieve this, we need to modify the `sync_taskwarrior.sh` script. We will add a check to see if there are any changes before committing and pushing to the repository.
    
    Here is the step-by-step explanation of the changes:
    
    1. After adding the files to the staging area with `git add .`, we will check if there are any changes using `git diff --staged --quiet`. This command returns a non-zero exit code if there are changes staged for commit.
    2. If there are changes, we will proceed with the commit and push. If there are no changes, we will print a message and skip the commit and push.
    
    Here is the *edit block* for the changes:
    
    ```bash
    sync_taskwarrior.sh
    <<<<<<< HEAD
    # Optionally, sync the data if you have a remote repository
    git -C "$DEST_DIR" add .
    git -C "$DEST_DIR" commit -m "Automated backup $(date)"
    git -C "$DEST_DIR" push origin main
    =======
    # Optionally, sync the data if you have a remote repository
    git -C "$DEST_DIR" add .
    if ! git -C "$DEST_DIR" diff --staged --quiet; then
        git -C "$DEST_DIR" commit -m "Automated backup $(date)"
        git -C "$DEST_DIR" push origin main
    else
        echo "No changes to commit"
    fi
    >>>>>>> updated
    ```

--- sync_taskwarrior.sh
+++ sync_taskwarrior.sh
@@ -12,6 +12,10 @@ cp -r "$SOURCE_DIR"/* "$DEST_DIR"/
 
 # Optionally, sync the data if you have a remote repository
 git -C "$DEST_DIR" add .
-git -C "$DEST_DIR" commit -m "Automated backup $(date)"
-git -C "$DEST_DIR" push origin main
+if ! git -C "$DEST_DIR" diff --staged --quiet; then
+    git -C "$DEST_DIR" commit -m "Automated backup $(date)"
+    git -C "$DEST_DIR" push origin main
+else
+    echo "No changes to commit"
+fi
 
